6e99f685c5e44af09c9a7fefbaba95dcfb4c8c6c
Luis F Cerda <luiscerdamun@gmail.com>
2024-02-10 15:38:18 -0300

    Added validation for the Identifier, IdentifierType, AttributeField and AttributeValue models. In IDType and AttrField the values have to be unique, and in the others the combination of their value with their parente 'type' now has to be unique as well

--- app/models/attribute_value.rb
+++ app/models/attribute_value.rb
@@ -1,3 +1,4 @@
 class AttributeValue < ApplicationRecord
   belongs_to :attribute_field
+  validates :description, uniqueness: { scope: :attribute_field_id }
 end
--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -1,4 +1,5 @@
 class Identifier < ApplicationRecord
   belongs_to :identifier_type
   belongs_to :identifiable, polymorphic: true
+  validates :value, uniqueness: { scope: :identifier_type_id }
 end
